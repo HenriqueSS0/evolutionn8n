71a1379baffd8f10113d686f3ea6d581bad949fe
HenrySS0 <87623476+HenriqueSS0@users.noreply.github.com>
2025-07-16 15:57:09 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Inicia Redis em background
 redis-server --daemonize yes
 
-echo "Redis iniciado"
-
-# Inicia Evolution API em background
-cd /evolution-api
-npm run start:prod &
-
-echo "Evolution API iniciado"
-
-# Dá um tempinho para Evolution subir
-sleep 10
-
-# Inicia n8n em background
-n8n start &
-
-echo "n8n iniciado"
-
-# Inicia nginx em foreground (container mantém ativo)
-nginx -g 'daemon off;'
+# Inicia n8n
+n8n
